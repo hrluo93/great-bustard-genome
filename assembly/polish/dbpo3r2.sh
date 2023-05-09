830ee52acb0b7518f203cc84b034bb40a8228d4e
hrluo93 <57522086+hrluo93@users.noreply.github.com>
2023-05-09 15:10:09 +0800

    nextpolish-round2 shell

---
+++ assembly/polish/dbpo3r2.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+#PBS -N db3por2
+#PBS -q batch
+#PBS -l nodes=1:ppn=6
+
+cd /home/tilapia/xuluohao/dabao/asm/
+/home/tilapia/srcs/NextPolish/nextPolish /home/tilapia/xuluohao/dabao/asm/dblgsporun2.cfg
