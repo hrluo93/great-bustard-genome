85b19a2b9942d9ee7097be971d1e87b2e603a7c5
hrluo93 <57522086+hrluo93@users.noreply.github.com>
2023-05-09 15:01:35 +0800

    Nextdenovo-run

---
+++ assembly/nextdb.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+#PBS -N nextdb
+#PBS -q batch
+#PBS -l nodes=1:ppn=48
+
+cd /home/tilapia/xuluohao/dabao/asm/
+/home/tilapia/srcs/NextDenovo/nextDenovo /home/tilapia/xuluohao/dabao/asm/rundb40k.cfg
